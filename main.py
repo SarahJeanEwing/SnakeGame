2c1a83216865230d4b1cb4231de2838bf013a5e6
Sarah Ewing <sarah.ewing@shyftsolutions.io>
2023-08-19 13:05:40 -0500

    added extra functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,7 +13,8 @@ screen.tracer(0)
 segments = []
 
 snake = Snake()
-
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,9 +26,20 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(.1)
-
     snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 
--- snake.py
+++ snake.py
@@ -16,13 +16,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
         #print(new_segment.heading())
 
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
     def move(self):
         # if self.segments[0].heading() = 0.0:
         #     y = self.segments[0].ycor()
